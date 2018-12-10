2f573333dc50127f3dc4a3e32a200a9e3d50ea7b
Wong Hoi Sing Edison <hswong3i@gmail.com>
2018-12-10 21:08:18 +0800

    3.3.2-1alvistack1

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -2,10 +2,5 @@
 
 set -o xtrace
 
-# Ensure required folders exist with correct owner:group
-mkdir -p $CROWD_HOME
-chown -Rf $CROWD_OWNER:$CROWD_GROUP $CROWD_HOME
-chmod 0755 $CROWD_HOME
-
 # Running Crowd in foreground
 exec /etc/init.d/crowd start -fg
